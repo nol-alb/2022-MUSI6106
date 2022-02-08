162c1f91060678a63af178a3b14f58c012d7a0af
nol-alb <noelalben@Noels-MacBook-Pro.local>
2022-02-08 01:36:40 -0500

    Combfilter.h is made, with brief methods

--- src/inc/CombFilter.h
+++ src/inc/CombFilter.h
@@ -8,11 +8,42 @@
 #ifndef CombFilter_h
 #define CombFilter_h
 #include "ErrorDef.h"
-// Base class definition for CombFilter
+// Base class definition for CombFilter FIR AND IIR (Brief definitions keep the implementation hidden)
 class CCombFilterBase
 {
-
+public:
+    CCombFilterBase(int iMaxDelayLengthInFrames, int iNumChannels);
+    //You don't need to instantiate an object to call this function, make destructor with virtual
+    // Make all the implementations hidden, make a seperate cpp file in CombFilter Source to run the functions
+    virtual ~CCombFilterBase();
+    // use :: to use the enums from CCombFilterIf
+    Error_t setParam (CCombFilterIf::FilterParam_t eParam, float fParamValue);
+    float   getParam (CCombFilterIf::FilterParam_t eParam) const;
+    // process needs to be called withuot an object, make virtual
+    virtual Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames);
+//Add members only accessed in the class definition or member classes ex:FIRFILTER,IIRFILTER
+protected:
+    CRingBuffer<float>  **m_ppCRingBuffer;
+    int     m_iNumChannels;
 };
+//Making a child class from CCombFilterBase for FIR
 
+class CCombFilterFIR : public CCombFilterBase
+{
+    //make constructor and destructor
+public:
+    CCombFilterFIR(int iMaxDelayLengthInFrames, int iNumChannels):CCombFilterBase(iMaxDelayLengthInFrames, iNumChannels) {};
+    virtual ~CCombFilterFIR();
+    Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames) override;
+};
+//Similar child classs for IIR filter
+class CCombFilterIIR : public CCombFilterBase
+{
+public:
+    CCombFilterIIR (int iMaxDelayLengthInFrames, int iNumChannels);
+    virtual ~CCombFilterIIR (){};
+
+    Error_t process (float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames) override;
+};
 
 #endif /* CombFilter_h */
--- src/inc/RingBuffer.h
+++ src/inc/RingBuffer.h
@@ -5,8 +5,159 @@
 //  Created by Noel  Alben on 2/7/22.
 //
 
-#ifndef RingBuffer_h
-#define RingBuffer_h
+#if !defined(__RingBuffer_hdr__)
+#define __RingBuffer_hdr__
 
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
 
-#endif /* RingBuffer_h */
+/*! \brief implement a circular buffer of type T
+*/
+template <class T>
+class CRingBuffer
+{
+public:
+    explicit CRingBuffer(int iBufferLengthInSamples) :
+        m_iBuffLength(iBufferLengthInSamples),
+        m_iReadIdx(0),
+        m_iWriteIdx(0),
+        m_buffptr(0)
+    
+    {
+        assert(iBufferLengthInSamples > 0);
+        m_buffptr = new T[m_iBuffLength];
+        
+
+        // allocate and init
+    }
+
+    virtual ~CRingBuffer()
+    {
+        // free memory
+        delete [] m_buffptr;
+        m_buffptr=0;
+    }
+
+    /*! add a new value of type T to write index and increment write index
+    \param tNewValue the new value
+    \return void
+    */
+    void putPostInc (T tNewValue)
+    {
+        put(tNewValue);
+        m_iWriteIdx = wraparound(++m_iWriteIdx);
+        
+        
+    }
+
+    /*! add a new value of type T to write index
+    \param tNewValue the new value
+    \return void
+    */
+    void put(T tNewValue)
+    {
+        m_buffptr[m_iWriteIdx]= tNewValue;
+    }
+    
+    /*! return the value at the current read index and increment the read pointer
+    \return float the value from the read index
+    */
+    T getPostInc()
+    {
+        float new_value = m_buffptr[m_iReadIdx];
+        m_iReadIdx = wraparound(++m_iReadIdx);
+        
+        return new_value;
+    }
+
+    /*! return the value at the current read index
+    \return float the value from the read index
+    */
+    T get() const
+    {
+        return m_buffptr[m_iReadIdx];
+    }
+    
+    /*! set buffer content and indices to 0
+    \return void
+    */
+    void reset()
+    {
+        m_iReadIdx=0;
+        m_iWriteIdx=0;
+        for(int i =0; i<=m_iBuffLength;i++)
+        {
+            m_buffptr[i]=0;
+        }
+        
+    }
+
+    /*! return the current index for writing/put
+    \return int
+    */
+    int getWriteIdx() const
+    {
+        return m_iWriteIdx;
+    }
+
+    /*! move the write index to a new position
+    \param iNewWriteIdx: new position
+    \return void
+    */
+    void setWriteIdx(int iNewWriteIdx)
+    {
+        m_iWriteIdx=iNewWriteIdx;
+    }
+
+    /*! return the current index for reading/get
+    \return int
+    */
+    int getReadIdx() const
+    {
+        return m_iReadIdx;
+    }
+
+    /*! move the read index to a new position
+    \param iNewReadIdx: new position
+    \return void
+    */
+    void setReadIdx(int iNewReadIdx)
+    {
+        m_iReadIdx=iNewReadIdx;
+    }
+
+    /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
+    \return int
+    */
+    int getNumValuesInBuffer() const
+    {
+        // (12-8+15)
+        return (m_iWriteIdx-m_iReadIdx + m_iBuffLength)%m_iBuffLength ;
+    }
+
+    /*! returns the length of the internal buffer
+    \return int
+    */
+    int getLength() const
+    {
+        return -1;
+    }
+private:
+    CRingBuffer();
+    CRingBuffer(const CRingBuffer& that);
+
+    int m_iBuffLength;              //!< length of the internal buffer
+    int m_iReadIdx;
+    int m_iWriteIdx;
+    
+    T *m_buffptr;
+    int wraparound(int Idx) {
+        int Idx_new = Idx%m_iBuffLength;
+        return Idx_new;
+        }
+    
+};
+#endif // __RingBuffer_hdr__
+
